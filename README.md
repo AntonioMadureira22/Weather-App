67a8befb3fbde2d1805a2a744e94c76a8c5b8699
antonio madureira <antonio.madureira2222@gmail.com>
2022-01-04 17:03:52 -0500

    added js and tried to fix some functions

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # Weather-App
 One stop shop for your 5-day forecast needs
+![Screenshot (10)](https://user-images.githubusercontent.com/86333093/148129777-d0b43959-ae50-4463-8bbc-8a2e682953d2.png)
 
 # Technology 
 HTML, CSS, JAVASCRIPT
+
+# Link
 
 # Use
 Need an app to tell you the weather from where you are? Look no further! This app allows you to give the forecast of any location with a click of a button!
 
 # License 
-NONE
+MIT License
 
 
--- js/script.js
+++ js/script.js
@@ -1,12 +1,48 @@
-function populate() {
-    var cityInput = document.getElementById("cityInput");
-    var cityName = document.getElementById("cityName");
-    cityName.innerHTML = "--"=cityInput.value+"--";
+function getInfo() {
+    const newName = document.getElementById("cityInput");
+    const cityName = document.getElementById("cityName");
+    cityName.innerHTML = "--" = newName.value + "--";
 
-    fetch("api.openweathermap.org/data/2.5/forecast?q=+cityInput&appid=93581f35c2cb6b03c064536e275ff676")
-    .then(Response => Response.json())
-    .then(data => {
 
-        
-    })
+    fetch("http://api.openweathermap.org/data/2.5/forecast?q='+newName.value+''&appid=a4aac97ac94b203e4f85929c9eaead8b")
+        .then(response => response.json())
+        .then(data => {
+            for (i = 0; i < 5; i++) {
+                document.getElementById("day" + (i + 1) + "Min").innerHTML = "Min:" + Number(data.list[i].main.temp_min - 275.19).toFixed(1) + "°";
+            }
+
+            for (i = 0; i < 5; i++) {
+                document.getElementById("day" + (i + 1) + "Max").innerHTML = "Max:" + Number(data.list[i].main.temp_Max - 275.44) .toFixed(2) + "°";
+            }
+
+            //Display weather icons from api
+            for (i = 0; i < 5; i++) {
+                document.getElementById("img" + (i + 1)).src = "http://openweathermap.org/img/wn/" + 
+                data.list[i].weather[0].icon + ".png";
+            }
+
+        })
+
+}
+//display default city upon open 
+function DefaultScreen() {
+    document.getElementById("cityInput").defaultValue = "Hartford";
+    getInfo();
+}
+
+const d = new Date();
+const weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday",];
+
+
+function CheckDay(day) {
+    if (day + d.getDay() > 6) {
+        return day + d.getDay() - 7;
+    }
+    else {
+        return day + d.getDay();
+    }
+}
+
+for (i = 0; i < 5; i++) {
+    document.getElementById("day" + (i + 1)).innerHTML = weekdays[CheckDay(i)];
 }
